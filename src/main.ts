db8fc74a9f95baa2d8cddd1ee77c1464e825d7cd
Russell Franz Vergara Rojas <sisalm0202@gmail.com>
2021-06-26 04:09:03 -0500

    main

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
     })
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('apirest', app, document);
   await app.listen(3000);
 }
 bootstrap();
