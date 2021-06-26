f55f18be65736f64e6c6c8a235f3379ca5d0756e
Russell Franz Vergara Rojas <sisalm0202@gmail.com>
2021-06-26 04:27:38 -0500

    Authmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
 import { PassportModule } from '@nestjs/passport/dist/passport.module';
+import { ConfigdbModule } from 'src/configdb/configdb.module';
+import { ConfigdbService } from 'src/configdb/configdb.service';
 import { MailTemplatesModule } from '../mail-templates/mail-templates.module';
 import { UsersModule } from '../system/users/users.module';
 import { TokensModule } from '../tokens/tokens.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { FacebookStrategy } from './strategy/facebook.strategy';
 import { GoogleStrategy } from './strategy/google.strategy';
@@ -15,12 +18,18 @@ import { JwtStrategy } from './strategy/jwt.strategy';
     MailTemplatesModule,
     TokensModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES },
+    JwtModule.registerAsync({
+      imports: [ConfigdbModule],
+      useFactory: async (configService:ConfigdbService) => {
+        return {
+          secret: process.env.JWT_SECRET_KEY,
+          signOptions: { expiresIn: process.env.JWT_EXPIRES },
+        }
+      }
     }),
+    ConfigdbModule
   ],
-  controllers: [],
+  controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy, PassportModule],
 })
