b497a4d8951eed4ed235157e6749f33f227fabbd
Russell <russellvergara@hotmail.com>
2021-07-03 13:40:30 -0500

    PASSPORT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
-import { PassportModule } from '@nestjs/passport/dist/passport.module';
 import { ConfigdbModule } from 'src/configdb/configdb.module';
 import { ConfigdbService } from 'src/configdb/configdb.service';
 import { MailTemplatesModule } from '../mail-templates/mail-templates.module';
@@ -10,8 +9,9 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { FacebookStrategy } from './strategy/facebook.strategy';
 import { GoogleStrategy } from './strategy/google.strategy';
-import { JwtStrategy } from './strategy/jwt.strategy';
 import 'dotenv/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
@@ -21,13 +21,13 @@ import 'dotenv/config';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigdbModule],
-      useFactory: async (configService:ConfigdbService) => {
+      useFactory: async (configService: ConfigdbService) => {
         return {
           secret: process.env.JWT_SECRET_KEY,
           signOptions: { expiresIn: process.env.JWT_EXPIRES },
-        }        
+        }
       },
-      inject:[ConfigdbService]
+      inject: [ConfigdbService]
     }),
     ConfigdbModule
   ],
@@ -35,4 +35,4 @@ import 'dotenv/config';
   providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy, PassportModule],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,10 +1,10 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtPayloadBase, JwtPayload } from './jwt-payload.interface';
 import { User } from 'src/system/users/users.entity';
 import { UsersService } from 'src/system/users/users.service';
 import 'dotenv/config';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(public usersService: UsersService) {
